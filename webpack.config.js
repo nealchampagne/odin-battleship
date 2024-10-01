4133c434da3f8b9daaf873b9e7c8bf4991ee7e48
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-10-01 13:05:12 -0700

    Tweak webpack.config file to use ES6 syntax

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 import path from "path";
+import { fileURLToPath } from "url";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
-module.exports = {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default{
   mode: "development",
   entry: "./src/index.js",
   plugins: [
