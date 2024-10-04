2730b45ab8932f971bd8323bc652e9c930f8f016
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-10-04 16:10:01 -0700

    Implement ship placement and attack reception
    Ships must be placed in empty spaces, and attacks increment a given
    ship's hit counter.

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,6 +1,77 @@
-export const Gameboard = () => {
-  
-  const gameArray = new Array(10).map(() => new Array(10));
+import { Ship } from './ship.js';
 
-  return { gameArray };
-}
+export const Gameboard = () => {
+
+  const boardObjects = [];
+  
+  const gridSize = 9;
+
+  const checkEmpty = (range, direction, x, y) => {
+    let isEmpty = true;
+
+    for (let i = 0; i < range; i++) {
+      boardObjects.forEach(obj => {
+        obj.positionArray.forEach(element => {
+          if (direction === 'horizontal') {
+            if (JSON.stringify(element) === `[${x+ i},${y}]`) {
+              isEmpty = false;
+            }
+          } else if (direction === 'vertical') {
+            if (JSON.stringify(element) === `[${x},${y+i}]`) {
+              isEmpty = false;
+            };
+          }
+        });
+      });
+    };
+    return isEmpty;
+  };
+
+  const placeShip = (size, direction, x , y) => {
+
+    const positionArray = [];
+    const ship = Ship(size);
+
+    if (!checkEmpty(size, direction, x, y)){
+      throw new Error('Cannot place ship in an occupied space');
+    } else {
+      if (direction === 'horizontal') {
+        if ((x + (size - 1)) > gridSize) {
+          throw new Error('Cannot place ship off the board');
+        } else {
+          for(let i = 0; i < size; i++) {
+            positionArray.push([x + i, y]);
+          };
+        };
+      } else if (direction === 'vertical') {
+        if ((y + (size -1 )) > gridSize) {
+          throw new Error('Cannot place ship off the board')
+        } else {
+          for(let i = 0; i < size-1; i++) {
+            positionArray.push([x, y + i]);
+          };
+        };
+      };
+
+      boardObjects.push({ positionArray, ship });
+
+      return { positionArray, ship }
+    };
+  };
+
+  const receiveAttack = (x, y) => {
+    let isHit = false;
+    boardObjects.forEach(obj => {
+      if (obj.positionArray.find(element => JSON.stringify(element) === `[${x},${y}]`)) {
+        obj.ship.hit();
+        isHit = true;
+      };
+    });
+    return isHit;
+  };
+
+  return { placeShip, 
+          checkEmpty,
+          boardObjects,
+          receiveAttack };
+};
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,5 +1,77 @@
 import { Gameboard } from './gameboard.js';
 
-test('Returns an object', () => {
+test('Gameboard returns an object', () => {
   expect(typeof Gameboard()).toBe('object');
 });
+
+test('placeShip returns an object', () => {
+  expect(typeof Gameboard().placeShip(5,'horizontal',1,1)).toBe('object');
+});
+
+test('positionArray of placeShip is an array', () => {
+  expect(typeof Gameboard().placeShip(5,'vertical',1,1).positionArray).toBe('object');
+});
+
+test('ship returned by placeShip is an object', () => {
+  expect(typeof Gameboard().placeShip(5,'horizontal',1,1).ship).toBe('object');
+});
+
+test('ship returned by placeShip has correct name', () => {
+  expect(Gameboard().placeShip(3,'horizontal',1,1).ship.name).toBe('cruiser');
+});
+
+test('Throws an error when ship is placed off the board', () => {
+  expect(() => Gameboard().placeShip(5,'horizontal',8,0))
+    .toThrow('Cannot place ship off the board');
+  expect(() => Gameboard().placeShip(5,'vertical',1,8))
+    .toThrow('Cannot place ship off the board');
+});
+
+test('positionArray of placeShip contains arrays', () => {
+  expect(typeof Gameboard().placeShip(5,'horizontal',1,1).positionArray[0])
+    .toBe('object');
+});
+
+test('positionArray of placeShip contains two-element arrays', () => {
+  expect(Gameboard().placeShip(5,'horizontal',1,1).positionArray[0].length)
+    .toBe(2);
+});
+
+test('positionArray length should be equal to ship size', () => {
+  expect(Gameboard().placeShip(5,'horizontal',1,1).positionArray.length)
+    .toBe(5);
+});
+
+test('positionArray should be filled with expected values', () => {
+  expect(JSON.stringify(Gameboard().placeShip(5,'horizontal',1,1).positionArray))
+    .toBe('[[1,1],[2,1],[3,1],[4,1],[5,1]]');
+});
+
+test('Throws appropriate error when placing a ship in an occupied space', () => {
+
+  const playerBoard = Gameboard();
+
+  playerBoard.placeShip(5,'horizontal',1,1);
+
+  expect(() => playerBoard.placeShip(5,'vertical',3,0))
+    .toThrow('Cannot place ship in an occupied space');
+  
+});
+
+test('receiveAttack takes a pair of coordinates and returns a boolean', () => {
+  expect(typeof Gameboard().receiveAttack(1,2)).toBe('boolean');
+});
+
+test('receiveAttack eventually results in a sunk ship', () => {
+  const playerBoard = Gameboard();
+
+  playerBoard.placeShip(2,'horizontal',1,1);
+
+  playerBoard.receiveAttack(1,1);
+
+  expect(playerBoard.boardObjects[0].ship.isSunk()).toBe(false);
+
+  playerBoard.receiveAttack(2,1);
+
+  expect(playerBoard.boardObjects[0].ship.isSunk()).toBe(true);
+})
