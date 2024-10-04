0806ad2e9cfe8675c7e64d330aa2c559d38482d8
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-10-04 16:08:54 -0700

    Implement and test ship name, hit, and sinking

--- src/ship.js
+++ src/ship.js
@@ -1,12 +1,14 @@
 export const Ship = size => {
 
-  const name = size => {
+  const getName = size => {
     if (size === 5) return 'carrier';
     if (size === 4) return 'battleship';
     if (size === 3) return 'cruiser';
-    if (size === 2) return 'submarine';
+    if (size === 2) return 'destroyer';
     if (size === 1) return 'patrol';
   }
+
+  const name = getName(size);
 
   let hitCount = 0;
 
@@ -18,5 +20,8 @@ export const Ship = size => {
     return hitCount === size ? true : false;
   }
 
-  return { name, hit, hitCount, isSunk }
+  return { name,
+          size,
+          hit,
+          isSunk }
 };
--- src/ship.test.js
+++ src/ship.test.js
@@ -1,4 +1,5 @@
 import { Ship } from './ship.js'
+import {jest} from '@jest/globals'
 
 test('returns an object', () => {
   expect(typeof Ship(5)).toBe('object');
@@ -7,3 +8,39 @@ test('returns an object', () => {
 test('Is not sunk on initialization', () => {
   expect(Ship(5).isSunk()).toBe(false);
 });
+
+test('Size maps to ship name', () => {
+  expect(Ship(3).name).toBe('cruiser');
+});
+
+jest.mock('./ship.js');
+
+test('A carrier (size 5) should be sunk after 5 hits.', () => {
+  const carrier = Ship(5);
+  
+  carrier.hit();
+  carrier.hit();
+  carrier.hit();
+  carrier.hit();
+  
+  expect(carrier.isSunk()).toBe(false);
+
+  carrier.hit();
+  
+  expect(carrier.isSunk()).toBe(true);
+});
+
+test('A battleship (size 4) should be sunk after 4 hits.', () => {
+  const battleship = Ship(4);
+  
+  battleship.hit();
+  battleship.hit();
+  battleship.hit();
+
+  
+  expect(battleship.isSunk()).toBe(false);
+
+  battleship.hit();
+  
+  expect(battleship.isSunk()).toBe(true);
+})
